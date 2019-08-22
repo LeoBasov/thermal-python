c7d6624eab24973a2ce49ebae8bed009ec56cf7e
leo <basov.leo@googlemail.com>
2019-08-22 11:25:00 +0200

    Added adding blocks.

--- app/main.py
+++ app/main.py
@@ -12,6 +12,10 @@ def main():
 
     doain = thermal.domain.Domain()
 
+    doain.add_block((0, 0), (1, 1))
+    #doain.add_block((-1, 0), (1, 1))
+    #doain.add_block((0, 0), (0, 1))
+
     print(80*'=')
 
 if __name__ == '__main__':
--- thermal/domain.py
+++ thermal/domain.py
@@ -6,8 +6,30 @@ from enum import Enum
 class Domain:
     def __init__(self):
         self.blocks = []
-        self.node_blocks = []
         self.nodes = []
+
+    def add_block(self, min, max):
+        self.check(min, max)
+
+        id = len(self.blocks)
+        block = Block(id, min, max)
+
+        side1 = Side((max[0], min[1]), max)
+        side2 = Side(max, (min[0], max[1]))
+        side3 = Side((min[0], max[1]), min)
+        side4 = Side(min, (max[0], min[1]))
+
+        block.set_sides(side1, side2, side3, side4)
+
+        self.blocks.append(block)
+
+        return id
+
+    def check(self, min, max):
+        if min[0] < 0 or min[1] < 0:
+            raise Exception('Domain', 'min[0]', min[0], 'min[1]', min[1])
+        elif max[0] <= min[0] or max[1] <= min[1]:
+            raise Exception('Domain', 'min[0]', min[0], 'min[1]', min[1], 'max[0]', max[0], 'max[1]', max[1])
 
 class Node:
     def __init__(self):
@@ -44,8 +66,9 @@ class Connection:
         self.side_id = side_id
 
 class Side:
-    def __init__(self, id, type, value = None):
-        self.id = id
-        self.type = type
-        self.value = value
-        self.connections = []
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+        self.type = None
+        self.value = None
+        self.connection = None
