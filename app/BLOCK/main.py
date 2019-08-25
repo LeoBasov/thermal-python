76f98762231d4beb4e5a115d9472b9d88ceb4a6e
Leo Basov <basov.leo@gmail.com>
2019-08-25 17:07:27 +0200

    Fixed block case.

--- app/BLOCK/main.py
+++ app/BLOCK/main.py
@@ -12,21 +12,22 @@ def main():
     print(80*'=')
     print('Thermal test')
 
-    CELL_SIZE = 0.001
-    DIFF_MAX = 1.0e-4
+    RAD_FRAC = 0.01
+    CELL_SIZE = 1e-3
+    DIFF_MAX = 1.0e-5
 
     domain = thermal.domain.Domain()
     sover = thermal.solver.Solver()
     visualizer = thermal.visualizer.Visualizer()
 
     #set blocks
-    block = domain.add_block((0, 1), (50, 50), conductivity = 1, density = 1, heat_capacity = 1)
+    block = domain.add_block((0, 10), (50, 60), conductivity = 1, density = 1, heat_capacity = 1)
 
     #set sides housing_1
     domain.set_side(block, 0, thermal.domain.Type.NEUMANN, value = 0.)
-    domain.set_side(block, 1, thermal.domain.Type.DIRICHLET, value = 100.0)
+    domain.set_side(block, 1, thermal.domain.Type.DIRICHLET, value = 1500.0)
     domain.set_side(block, 2, thermal.domain.Type.NEUMANN, value = 0.0)
-    domain.set_side(block, 3, thermal.domain.Type.DIRICHLET, value = 10000.0)
+    domain.set_side(block, 3, thermal.domain.Type.BLACK_BODY, value = 300.0)
 
     print(80*'-')
     print('ASSEMBLING NODES')
@@ -36,7 +37,7 @@ def main():
     print('ASSEMBLING NUMERICS')
     sover.assemble(domain, CELL_SIZE)
     print('SOLVING NUMERICS')
-    sover.solve(DIFF_MAX)
+    sover.solve(DIFF_MAX, RAD_FRAC)
     sover.get_results(domain)
 
     visualizer.plot(domain, CELL_SIZE)
