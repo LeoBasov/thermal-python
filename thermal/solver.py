26f242c29bfa09a3496be3be67a9490087024f37
Leo Basov <basov.leo@gmail.com>
2019-08-28 11:17:06 +0200

    Modified radiotion boundary calculation.

--- thermal/solver.py
+++ thermal/solver.py
@@ -3,6 +3,7 @@ import math
 from .domain import Type
 from .domain import SideType
 import sys
+from .rad_solver import calc_T
 
 BOLTZMANN_CONST = 5.670374419e-8 #W m^−2⋅K^−4
 
@@ -13,14 +14,18 @@ class Solver:
         self.vector_add = None
 
         self.vector_rad = None
-        self.vector_rad_const = None
+        self.vector_rad_index_pairs = None
         self.vector_background_temp_4 = None
+
+        self.cell_size = None
 
     def assemble(self, domain, cell_size):
         self._assemble_vector(domain)
         self._assemble_matrix(domain, cell_size)
         self._assemble_vector_add(domain, cell_size)
         self._assemble_vector_rad(domain, cell_size)
+
+        self.cell_size = cell_size
 
     def _assemble_vector(self, domain):
         self.vector = np.zeros(len(domain.nodes))
@@ -58,7 +63,7 @@ class Solver:
                 self.matrix[row][node_z_rp[0]] = 0.25 + (k_z_rp - k_z_rm)/(16.0*k) + cell_size/(8.0*r_i)
                 self.matrix[row][node_z_rm[0]] = 0.25 - (k_z_rp - k_z_rm)/(16.0*k) - cell_size/(8.0*r_i)
 
-            elif node.type == Type.NEUMANN or node.type == Type.BLACK_BODY:
+            elif node.type == Type.NEUMANN:
                 if node.side_type == SideType.UP:
                     node_z_rm = domain.find_node((node.pos[0], node.pos[1] - 1))
                     self.matrix[row][node_z_rm[0]] = 1.0
@@ -118,13 +123,30 @@ class Solver:
 
     def _assemble_vector_rad(self, domain, cell_size):
         self.vector_rad = np.zeros(len(domain.nodes))
-        self.vector_rad_const = len(domain.nodes)*[None]
+        self.vector_rad_index_pairs = [[False, 0, 0] for _ in range(len(domain.nodes))]
         self.vector_background_temp_4 = len(domain.nodes)*[None]
 
         for i in range(len(domain.nodes)):
             if domain.nodes[i].type == Type.BLACK_BODY:
-                self.vector_rad_const[i] = (-1.0)*cell_size*BOLTZMANN_CONST/domain.nodes[i].conductivity
+                node = domain.nodes[i]
+
+                node_zp_r = domain.find_node((node.pos[0] + 1, node.pos[1]))
+                node_zm_r = domain.find_node((node.pos[0] - 1, node.pos[1]))
+                node_z_rp = domain.find_node((node.pos[0], node.pos[1] + 1))
+                node_z_rm = domain.find_node((node.pos[0], node.pos[1] - 1))
+
                 self.vector_background_temp_4[i] = math.pow(domain.nodes[i].background_temp, 4)
+                self.vector_rad_index_pairs[i][0] = True
+                self.vector_rad_index_pairs[i][2] = domain.nodes[i].conductivity
+
+                if domain.nodes[i].side_type == SideType.RIGHT:
+                    self.vector_rad_index_pairs[i][1] = node_zm_r[0]
+                elif domain.nodes[i].side_type == SideType.UP:
+                    self.vector_rad_index_pairs[i][1] = node_z_rm[0]
+                elif domain.nodes[i].side_type == SideType.LEFT:
+                    self.vector_rad_index_pairs[i][1] = node_zp_r[0]
+                elif domain.nodes[i].side_type == SideType.DOWN:
+                    self.vector_rad_index_pairs[i][1] = node_z_rp[0]
 
     def solve(self, diff_frac_max, rad_frac):
         itter = 0
@@ -150,14 +172,13 @@ class Solver:
                 diff_abs_old = diff_abs
 
     def _get_rad_values(self, rad_frac):
-        for i in range(len(self.vector_rad_const)):
-            if self.vector_rad_const[i]:
-                self.vector_rad[i] = self.vector_rad_const[i]*(math.pow(self.vector[i],4) - self.vector_background_temp_4[i])
+        for i in range(len(self.vector_rad_index_pairs)):
+            if self.vector_rad_index_pairs[i][0]:
+                q = self.vector_rad_index_pairs[i][2]/(BOLTZMANN_CONST*self.cell_size )
+                c = self.vector_background_temp_4[i] + q*self.vector[self.vector_rad_index_pairs[i][1]]
+                T = calc_T(q, c)
 
-                if self.vector_rad[i] > 0.0:
-                    self.vector_rad[i] = min(self.vector_rad[i],  rad_frac*abs(self.vector[i]))
-                elif self.vector_rad[i] < 0.0:
-                    self.vector_rad[i] = max(self.vector_rad[i], -rad_frac*abs(self.vector[i]))
+                self.vector_rad[i] = T
 
     def get_results(self, domain):
         for i in range(len(domain.nodes)):
