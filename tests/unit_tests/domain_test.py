492dc407e5accb0a4ba148cd3980ef3d33b26bd2
leo <basov.leo@googlemail.com>
2019-08-29 10:42:44 +0200

    Fixed unit tests.

--- tests/unit_tests/domain_test.py
+++ tests/unit_tests/domain_test.py
@@ -3,10 +3,10 @@ import sys
 
 sys.path.append('../.')
 
-import domain
+from thermal import domain
 
 class DomainTest(unittest.TestCase):
 	def test_init(self):
-		block = domain.Block(0, (0,0), (1,1))
+		block = domain.Block(0, (0,0), (1,1), conductivity = 1, density = 1, heat_capacity = 1)
 		connection = domain.Connection(0, 1)
 		side = domain.Side(0, domain.Type.CONNECTION)
